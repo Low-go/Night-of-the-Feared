3b49f8248e99347f153e6a6762d8887b4b5c9df7
Low-go <lorranalvesgaldino98@gmail.com>
2024-10-25 22:05:17 -1000

    finish maze generator script

--- Assets/Scripts/MazeGenerator.cs
+++ Assets/Scripts/MazeGenerator.cs
@@ -6,13 +6,13 @@ public class MazeGenerator : MonoBehaviour
 {
 
     [Range(5, 500)]
-    public int mazeWidth = 5, mazeHeight = 5;   // dimensions
+    public int mazeWidth = 20, mazeHeight = 30;   // dimensions
     public int startX, startY;
     MazeCell[,] maze;
 
     Vector2Int currentCell;
 
-    private void Start()
+    public MazeCell[,] GetMaze()
     {
         maze = new MazeCell[mazeWidth, mazeHeight];
 
@@ -23,6 +23,11 @@ public class MazeGenerator : MonoBehaviour
                 maze[x, y] = new MazeCell(x, y);
             }
         }
+
+        //start carving path
+        CarvePath(startX, startY);
+
+        return maze;
     }
 
     // Update is called once per frame
@@ -60,6 +65,110 @@ public class MazeGenerator : MonoBehaviour
         else return true;
     }
 
+
+    Vector2Int CheckNeighbours()
+    {
+        List<Direction> rndDir = GetRandomDirections();
+
+        for (int i = 0; i < rndDir.Count; i++)
+        {
+
+            Vector2Int neighbour = currentCell;
+
+            //modify neighbour coordinates depending on where we are at
+            switch (rndDir[i])
+            {
+
+                case Direction.Up:
+                    neighbour.y++;
+                    break;
+                case Direction.Down:
+                    neighbour.y--;
+                    break;
+                case Direction.Right:
+                    neighbour.x++;
+                    break;
+                case Direction.Left:
+                    neighbour.x--;
+                    break;
+
+            }
+
+            if (IsCellValid(neighbour.x, neighbour.y)) return neighbour;
+
+        }
+
+        return currentCell;
+    }
+
+    //works out which walls we are going to break at a time
+    void BreakWalls(Vector2Int primaryCell, Vector2Int secondaryCell)
+    {
+        if(primaryCell.x > secondaryCell.x){ //primary cell's left wall
+            maze[primaryCell.x, primaryCell.y].leftWall = false;
+        }
+        else if (primaryCell.x < secondaryCell.x)
+        {
+            maze[secondaryCell.x, secondaryCell.y].leftWall = false;
+        }
+        else if (primaryCell.y < secondaryCell.y)
+        { 
+            maze[primaryCell.x, primaryCell.y].topWall = false;
+        }
+        else if (primaryCell.y > secondaryCell.y)
+        {
+            maze[secondaryCell.x, secondaryCell.y].topWall = false;
+        }
+    }
+
+    void CarvePath(int x, int y)
+    {
+        if (x <0 || y < 0 || x > mazeWidth -1 || y > mazeWidth - 1) //just checking if they gave negative number, if so we throw to 0
+        {
+            x = y = 0;
+            Debug.LogWarning("Starting position is out of bounds, defaulting to 0, 0");
+        }
+
+        // set current cell to starting position we were passed
+        currentCell = new Vector2Int(x, y);
+
+        List<Vector2Int> path = new List<Vector2Int>();
+        
+
+        // loop until we hit a dead end
+        bool deadEnd = false;
+
+        while (!deadEnd)
+        {
+            //we will try the next cell
+            Vector2Int nextCell = CheckNeighbours(); // function call
+
+            if (nextCell == currentCell)
+            {
+                // if the cell has no valid neighbours set deadend to true so we break out of loop
+                for (int i = path.Count - 1; i >= 0; i--)
+                {
+                    currentCell = path[i];
+                    path.RemoveAt(i);
+                    nextCell = CheckNeighbours();
+
+                    //if we find valid neighbours break out of loop
+                    if (nextCell != currentCell) break;
+                }
+
+                if (nextCell == currentCell) {
+                    deadEnd = true;
+                }
+            }
+            else
+            {
+                BreakWalls(currentCell, nextCell); // set wall flags
+                maze[currentCell.x, currentCell.y].visited = true; // set cells visited before moving on
+                currentCell = nextCell;
+                path.Add(currentCell);  // add cell to path
+            }
+        }
+    }
 
 }
 
