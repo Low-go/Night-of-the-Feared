d6b082be8d6d36ea6f3c7bb7ba5db54b79a7a796
Low-go <lorranalvesgaldino98@gmail.com>
2024-10-31 17:08:19 -1000

    fix gravity issues, add monster prefab

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,44 +4,27 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-
     private CharacterController controller;
     public Camera playerCamera;
     public float speed = 5f;
-    public float gravity = -9.81f;
     public float mouseSensitivity;
-    private Vector3 velocity;
     private float xRotation = 0f;
     public GameObject spotlight;
 
     void Start()
     {
-        // locks the curso so it cant be seen as is custom in fps games
+        // locks the cursor so it cant be seen as is custom in fps games
         Cursor.lockState = CursorLockMode.Locked;
         controller = GetComponent<CharacterController>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal"); // get both axis's
+        // Movement
+        float horizontal = Input.GetAxis("Horizontal");
         float vertical = Input.GetAxis("Vertical");
         Vector3 move = transform.right * horizontal + transform.forward * vertical;
         controller.Move(move * speed * Time.deltaTime);
-
-        // gravity? hopefully. Maybe delete for now if this dosent work
-
-        if (controller.isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        else
-        {
-            velocity.y += gravity * Time.deltaTime;
-        }
-        controller.Move(velocity * Time.deltaTime);
-
 
         // Mouse Input for Camera Rotation
         float mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
@@ -49,10 +32,10 @@ public class PlayerMovement : MonoBehaviour
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f); // Prevent flipping
-
         playerCamera.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         transform.Rotate(Vector3.up * mouseX);
 
+        // Flashlight toggle
         if (Input.GetMouseButtonDown(0))
         {
             if (spotlight != null)
